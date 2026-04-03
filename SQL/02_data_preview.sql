c71187f0e271740dac53f26eaa56e4edaadde0e1
Shreya Patil <117198903+Shreyapatil1646@users.noreply.github.com>
2026-04-03 17:28:06 +0530

    Update and rename SQLQuery1.sql to 02_data_preview.sql

---
+++ SQL/02_data_preview.sql
@@ -0,0 +1,8 @@
+-- ============================================
+-- Project: Customer Churn Analysis
+-- Author: Shreya Patil
+-- Description: Previewing raw data from staging table
+-- ============================================
+
+SELECT *
+FROM stg_Churn;
--- SQL/SQLQuery1.sql
+++
@@ -1,1 +0,0 @@
-Select * from stg_Churn
